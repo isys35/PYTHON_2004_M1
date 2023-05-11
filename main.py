a6c516f31abcc1b049d99f12693f2958f946192a
poles <polesmen@gmail.com>
2023-05-11 20:45:48 +0300

    homework

---
+++ main.py
@@ -0,0 +1,18 @@
+summa_zakaza = int(input('Enter your burger price : '))
+
+tips = (summa_zakaza*18) / 100
+print('Tips :', tips)
+
+tax = (summa_zakaza * 4) / 100
+print('Tax :', tax)
+
+total = (summa_zakaza + tips+ tax)
+print('Total :', total)
+
+print("Thank you, come again")
+#comment
+
+#poles
+def create_task():
+    pass
+
