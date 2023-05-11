158259aacfcf45a20b9af5baf3606005dbd7e1bf
poles <polesmen@gmail.com>
2023-05-11 21:32:57 +0300

    poles_hw2

---
+++ poles_hw_2/main.py
@@ -0,0 +1,12 @@
+input_feet = int(input('Enter value in feet : '))
+
+inches = (input_feet*12)
+print('Inches :', inches)
+
+jards = (input_feet * 0.33)
+print('Jards :', jards)
+
+miles = (input_feet * 0.00018939)
+print('Miles :', miles)
+
+print("Easy breazy:))")
