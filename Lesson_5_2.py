160711eb032b685ae80529f42b11eb753116e132
Evgeniy997 <127623879+Evgeniy997@users.noreply.github.com>
2023-05-18 17:00:56 +0300

    Add files via upload
    Выполненное задание

---
+++ Lesson_5_2.py
@@ -0,0 +1,17 @@
+num = int(input("Введите число от 1 до 7:"))
+if num == 1:
+    print("Понедельник")
+elif num == 2:
+    print("Вторник")
+elif num == 3:
+    print("Среда")
+elif num == 4:
+    print("Четверг")
+elif num == 5:
+    print("Пятница")
+elif num == 6:
+    print("Суббота")
+elif num == 7:
+    print("Четверг")
+else:
+    print("Ошибка: введите число от 1 до 7")
