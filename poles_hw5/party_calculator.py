d16cb80bef615df2b86fd349dfac4348ffe42cb2
poles <polesmen@gmail.com>
2023-05-23 21:41:49 +0300

    task_2

---
+++ poles_hw5/party_calculator.py
@@ -0,0 +1,13 @@
+MAX_PEOPLE = 10
+user_input = int(input("Введите оличество гостей: "))
+guests_count = list(range(0, user_input))
+# print(guest_count)
+
+if user_input >= MAX_PEOPLE:
+    print("Too many people")
+else:
+    for guests in guests_count:
+        guest_name = str(input("Имя вашего гостя: "))
+        print(f"'{guest_name}' has been invited")
+
+# Тут есть вопрос. Rак красиво закончить программу после выполнения цикла?
