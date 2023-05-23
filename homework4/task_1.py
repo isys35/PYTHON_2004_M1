f1797b92f16bdfd11e42ca25f49ffbc5d7a7208b
AsmodeySF <asmodeysf@gmail.com>
2023-05-23 14:34:08 +0300

    hw4 one more atteÑmpt

--- homework4/task_1.py
+++ homework4/task_1.py
@@ -12,5 +12,5 @@ for writers, product in dict_test.items():
     answer = input(f"Кто написал произведение {product}? - ").title()
     if answer == writers:
         right += 1
-    else: right = right
+
 print(f"Правильных ответов: {right}")
