aababd7c95a641db63a329e44d330a255649e5e9
poles <polesmen@gmail.com>
2023-06-07 20:54:29 +0300

    decorator

---
+++ poles_hw_8/task_2_decorator.py
@@ -0,0 +1,13 @@
+
+def get_input_func():
+    user_input = input('Введите строку для обертки: ')
+    return user_input
+
+def html_decorator(function):
+    def html_wrapper():
+        return f"<html>{function}<html>"
+    return html_wrapper
+
+
+print(html_decorator(get_input_func()))
+
