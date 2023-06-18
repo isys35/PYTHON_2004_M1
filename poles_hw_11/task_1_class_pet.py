f365794f6eda9056e415d100df7a1eb6ad13061c
poles <polesmen@gmail.com>
2023-06-18 16:25:08 +0300

    default_names_added

--- poles_hw_11/task_1_class_pet.py
+++ poles_hw_11/task_1_class_pet.py
@@ -1,18 +1,18 @@
 class Pet:
     '''Описание класса питомец'''
-    def __init__(self, name, animal_type, age):
+    def __init__(self, name='default_name', animal_type='default_type', age='default_age'):
         self.name = name
         self.animal_type = animal_type
         self.age = age
 
-    def set_name(self,name):
+    def set_name(self, name):
          self.name = name
 
-    def set_animal_type(self,type):
+    def set_animal_type(self, type):
          self.type = type
          self.animal_type = self.type
          return self.animal_type
-    def set_age(self,age):
+    def set_age(self, age):
         self.age = age
         return self.age
 
@@ -26,14 +26,15 @@ class Pet:
 
 if __name__ == '__main__':
     def main():
+        mypet = Pet()
         user_input_name = input("Введите кличку Вашего питомца: ")
         user_input_type = input("Введите тип питомца(Котик, Собака, Черепашка и т.д.: ")
-        user_input_age = int(input("Введите возраст вашего питомца"))
-        mypet = Pet(user_input_name, user_input_type, user_input_age)
+        user_input_age = (input("Введите возраст вашего питомца"))
         mypet.set_name(user_input_name)
         mypet.set_animal_type(user_input_type)
         mypet.set_age(user_input_age)
-        print("Личная карточка Вашего питомца\n"
+        print("______________________________\n"
+              "Личная карточка Вашего питомца:\n"
               "______________________________")
         print(mypet.get_name())
         print(mypet.get_animal_type())
